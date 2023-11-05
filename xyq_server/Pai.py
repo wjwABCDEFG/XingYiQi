ae19e1c81938595d2e952b0ed7a6e7924c4ccc0e
wjwABCDEFG <975504808@qq.com>
2023-11-05 21:42:32 +0800

    [MOD]修改牌堆逻辑，牌的获取改为json配置方式

--- xyq_server/Pai.py
+++ xyq_server/Pai.py
@@ -4,98 +4,79 @@ Pai打出的牌
 5 * 5
 mask矩阵
 """
+import json
 import random
+from collections import OrderedDict
+
+from common.Serializable import Serializable
 
 
-class PaiDui:
+class PaiDui(Serializable):
 
-    def __init__(self, pai_list):
-        if len(pai_list) < 5:
-            raise RuntimeError("牌库太少了，玩不了")
+    def __init__(self, game):
+        super().__init__()
+        self.player_card_num = 2     # 每个玩家的起始手牌
+        self.game = game
         self.remain_list = []
         self.drop_list = []
-        self.reset(pai_list)
-        self.begin()
+        self._load()
+        self.reset()
 
-    def reset(self, pai_list):
-        self.remain_list = pai_list
+    def _load(self):
+        pai_list = []
+        with open("./pai.json") as f:
+            pai_info = json.loads(f.read())
+        for key, val in pai_info.items():
+            pai_list.append(Pai(val, key))
+        if len(pai_list) < 5:
+            raise RuntimeError("牌库太少了，玩不了")
+        self.remain_list = pai_list + pai_list
+
+    def reset(self):
+        if not self.remain_list and self.drop_list:
+            self.remain_list = self.drop_list
         random.shuffle(self.remain_list)
         self.drop_list = []
 
-    def begin(self):
+    def deal_cards(self):
         """
         发牌给两个玩家
         """
-        res = [[], []]
-        for i in range(4):
-            item = self.remain_list.pop(random.randrange(len(self.remain_list)))
-            res[i % 2].append(item)
-        return res
+        divide = [[], []]
+        for i in range(2 * self.player_card_num):
+            divide[i % 2].append(self.remain_list.pop())
+        self.game.player1.pai_list = divide[0]
+        self.game.player2.pai_list = divide[1]
 
     def take(self):
+        """
+        出牌
+        """
         if not self.remain_list and self.drop_list:
-            self.reset(self.drop_list)
-        return self.remain_list.pop()
+            self.reset()
+        top = self.remain_list.pop()
+        self.drop_list.append(top)
+        return top
 
-    def drop(self, pai):
-        self.drop_list.append(pai)
-
-
-class Pai:
-    def __init__(self):
-        self.matrix = [[0 for j in range(5)] for i in range(5)]
+    def serialize(self) -> OrderedDict:
+        return super().serialize()
 
 
-class Pai1(Pai):
-    def __init__(self):
+class Pai(Serializable):
+
+    def __init__(self, data, name='UntitledCard'):
         super().__init__()
-        self.matrix = [
-            [0, 0, 0, 0, 0],
-            [0, 0, 1, 0, 0],
-            [1, 0, 0, 0, 1],
-            [0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0]]
+        self.name = name
+        self.matrix = data
+
+    def serialize(self) -> OrderedDict:
+        return OrderedDict([
+            ("info", self.matrix),
+        ])
+
+    def deserialize(self, data: dict, hashmap: dict = {}, restore_id: bool = True) -> bool:
+        return super().deserialize(data, hashmap, restore_id)
 
 
-class Pai2(Pai):
-    def __init__(self):
-        super().__init__()
-        self.matrix = [
-            [0, 0, 0, 0, 0],
-            [0, 0, 1, 0, 0],
-            [0, 1, 0, 0, 0],
-            [0, 0, 1, 0, 0],
-            [0, 0, 0, 0, 0]]
-
-
-class Pai3(Pai):
-    def __init__(self):
-        super().__init__()
-        self.matrix = [
-            [0, 0, 0, 0, 0],
-            [0, 1, 0, 0, 0],
-            [0, 1, 0, 1, 0],
-            [0, 0, 0, 1, 0],
-            [0, 0, 0, 0, 0]]
-
-
-class Pai4(Pai):
-    def __init__(self):
-        super().__init__()
-        self.matrix = [
-            [0, 0, 0, 0, 0],
-            [0, 1, 0, 0, 0],
-            [1, 0, 0, 0, 0],
-            [0, 0, 0, 1, 0],
-            [0, 0, 0, 0, 0]]
-
-
-class Pai5(Pai):
-    def __init__(self):
-        super().__init__()
-        self.matrix = [
-            [0, 0, 0, 0, 0],
-            [0, 0, 0, 1, 0],
-            [0, 0, 0, 0, 1],
-            [0, 1, 0, 0, 0],
-            [0, 0, 0, 0, 0]]
+if __name__ == '__main__':
+    paidui = PaiDui()
